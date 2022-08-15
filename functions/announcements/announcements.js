69d7395cbdc206286cbe2dfce84ffcc4a68ff73d
Rudolf Hersén <rudolf.hersen@schibsted.com>
2022-08-15 09:18:13 +0200

    split LocationSignature

--- functions/announcements/announcements.js
+++ functions/announcements/announcements.js
@@ -1,5 +1,5 @@
 const fetch = require("node-fetch");
-exports.handler = async function({ queryStringParameters }) {
+exports.handler = async function ({ queryStringParameters }) {
   try {
     const response = await fetch(
       "https://api.trafikinfo.trafikverket.se/v2/data.json",
@@ -8,15 +8,15 @@ exports.handler = async function({ queryStringParameters }) {
         body: getBody(queryStringParameters),
         headers: {
           "Content-Type": "application/xml",
-          Accept: "application/json"
-        }
+          Accept: "application/json",
+        },
       }
     );
     if (!response.ok)
       // NOT res.status >= 200 && res.status < 300
       return {
         statusCode: response.status,
-        body: JSON.stringify({ msg: response.statusText })
+        body: JSON.stringify({ msg: response.statusText }),
       };
 
     const data = await response.json();
@@ -24,16 +24,24 @@ exports.handler = async function({ queryStringParameters }) {
 
     return {
       statusCode: 200,
-      body: JSON.stringify(body)
+      body: JSON.stringify(body),
     };
   } catch (err) {
     console.log(err); // output to netlify function log
     return {
       statusCode: 500,
-      body: JSON.stringify({ msg: err.message }) // Could be a custom message or object i.e. JSON.stringify(err)
+      body: JSON.stringify({ msg: err.message }), // Could be a custom message or object i.e. JSON.stringify(err)
     };
   }
 };
+
+const locationSignature = [
+  "Söd,Öte,Rön,Tu,Tul,Flb,Hu,Sta",
+  "Äs,Åbe,Sst,Sci,Sod",
+  "So,Udl,Hel,Sol,Hgv,Nvk,R,Upv,Rs,Mr",
+  "Nyc,Gdv,Ngd,Öso,Ss,Hfa,Ts,Kda,Vhe,Jbo,Hnd,Vga,Skg,Tåd,Fas",
+  "Sub,Spå,Bkb,Jkb,Khä",
+];
 
 function getBody({ direction, since }) {
   return `
@@ -43,7 +51,9 @@ function getBody({ direction, since }) {
       <FILTER>
         <OR>
           <AND>
-            <IN name='LocationSignature' value='Äs,Åbe,Sst,Sci,Sod,So,Udl,Hel,Sol,Hgv,Nvk,R,Upv,Rs,Mr,Söd,Öte,Rön,Tu,Tul,Flb,Hu,Sta,Nyc,Gdv,Ngd,Öso,Ss,Hfa,Ts,Kda,Vhe,Jbo,Hnd,Vga,Skg,Tåd,Fas,Sub,Spå,Bkb,Jkb,Khä' />
+            <IN name='LocationSignature' value='${locationSignature.join(
+              ","
+            )}' />
             <EQ name='ProductInformation.Code' value='PNA054' />
           </AND>
           <IN name='LocationSignature' value='Tmö,Kmy,Skby,Bra,Gau,Södy,Uts,Kng,Hön,Huv,Duo' />
